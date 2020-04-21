2ad3d46f745540693efdbd255975447226ea8fcb
Patrick Wang <patrick@covar.com>
2020-04-21 13:31:40 -0400

    shuffle things around to make them more readable/testable
    Answer writing is no longer batched. It does not seem to have been a bottleneck.

--- strider/fetcher.py
+++ strider/fetcher.py
@@ -194,23 +194,27 @@ class Fetcher(Worker, Neo4jMixin, RedisMixin, SqliteMixin):
         """Process response from KP."""
         if response is None:
             return
-        nodes_by_id = {
-            node['id']: node
-            for node in response['knowledge_graph']['nodes']
+        response['knowledge_graph'] = {
+            'nodes': {
+                node['id']: node
+                for node in response['knowledge_graph']['nodes']
+            },
+            'edges': {
+                edge['id']: edge
+                for edge in response['knowledge_graph']['edges']
+            },
         }
-        edges_by_id = {
-            edge['id']: edge
-            for edge in response['knowledge_graph']['edges']
-        }
+        knodes = response['knowledge_graph']['nodes']
+        kedges = response['knowledge_graph']['edges']
         # process all edges, in parallel
         edge_awaitables = []
         for result in response['results']:
             edge_bindings = {
-                binding['qg_id']: edges_by_id[binding['kg_id']]
+                binding['qg_id']: kedges[binding['kg_id']]
                 for binding in result['edge_bindings']
             }
             node_bindings = {
-                binding['qg_id']: nodes_by_id[binding['kg_id']]
+                binding['qg_id']: knodes[binding['kg_id']]
                 for binding in result['node_bindings']
             }
             edge_awaitables.append(self.process_result(
@@ -242,10 +246,6 @@ class Fetcher(Worker, Neo4jMixin, RedisMixin, SqliteMixin):
         )
 
         # get subgraphs and scores
-        qgraph = query.qgraph
-        qnode_ids = list(qgraph['nodes'])
-        qedge_ids = list(qgraph['edges'])
-
         subgraphs = await self.get_subgraphs(
             query,
             new_edges,
@@ -258,33 +258,30 @@ class Fetcher(Worker, Neo4jMixin, RedisMixin, SqliteMixin):
             },
             'edges': {},
         })
-        scores = await asyncio.gather(*[
-            score_graph(subgraph, qgraph, **kwargs)
+        await asyncio.gather(*[
+            self.process_subgraph(query, job_id, subgraph, **kwargs)
             for subgraph in subgraphs
         ])
-
-        # update priorities
-        await self.update_priorities(query, subgraphs, scores)
-
-        # publish answers to the results DB
-        answers = [
-            (subgraph, score)
-            for subgraph, score in zip(subgraphs, scores)
-            if (
-                set(subgraph['nodes'].keys()) == set(qnode_ids)
-                and set(subgraph['edges'].keys()) == set(qedge_ids)
-            )
-        ]
-        await self.store_answers(
-            query, job_id,
-            answers,
-        )
 
         # publish all nodes to jobs queue
         await self.queue_jobs(query, node_bindings, edge_bindings, job_id)
 
         # black-list any old jobs for these nodes
         # *not necessary if priority only increases
+
+    async def process_subgraph(self, query, job_id, subgraph, **kwargs):
+        """Process subgraph."""
+        score = await score_graph(subgraph, query.qgraph, **kwargs)
+        await self.update_priorities(query, subgraph, score)
+
+        if (
+                set(subgraph['nodes'].keys()) == set(query.qgraph['nodes'])
+                and set(subgraph['edges'].keys()) == set(query.qgraph['edges'])
+        ):
+            await self.store_answer(
+                query, job_id,
+                subgraph, score,
+            )
 
     async def validate_result(
             self,
@@ -306,15 +303,14 @@ class Fetcher(Worker, Neo4jMixin, RedisMixin, SqliteMixin):
             target_spec = query.qgraph['nodes'][qid]
             await self.validate(node, target_spec)
 
-    async def update_priorities(self, query, subgraphs, scores):
+    async def update_priorities(self, query, subgraph, score):
         """Update job priorities."""
-        # for each subgraph, add its weight to each component node's priority
-        for subgraph, score in zip(subgraphs, scores):
-            for node in subgraph['nodes'].values():
-                await query.update_priority(
-                    f'({node["qid"]}:{node["kid"]})',
-                    score
-                )
+        # add the subgraph weight to each node's priority
+        for node in subgraph['nodes'].values():
+            await query.update_priority(
+                f'({node["qid"]}:{node["kid"]})',
+                score
+            )
 
     async def get_subgraphs(self, query, new_edges):
         """Get subgraphs."""
@@ -454,26 +450,23 @@ class Fetcher(Worker, Neo4jMixin, RedisMixin, SqliteMixin):
         result = await self.neo4j.run_async(statement)
         return [row['e'] for row in result]
 
-    async def store_answers(self, query, job_id, answers):
+    async def store_answer(self, query, job_id, answer, score):
         """Store answers in sqlite."""
-        if not answers:
-            return
         rows = []
         start_time = await query.get_start_time()
         slots = list(query.qgraph['nodes']) + list(query.qgraph['edges'])
-        for answer, score in answers:
-            things = {**answer['nodes'], **answer['edges']}
-            values = (
-                [json.dumps(things[qid]) for qid in slots]
-                + [score, time.time() - start_time]
-            )
-            rows.append(values)
-            LOGGER.debug(
-                "[query %s]: [job %s]: Storing answer %s",
-                query.uid,
-                job_id,
-                str(values),
-            )
+        things = {**answer['nodes'], **answer['edges']}
+        values = (
+            [json.dumps(things[qid]) for qid in slots]
+            + [score, time.time() - start_time]
+        )
+        rows.append(values)
+        LOGGER.debug(
+            "[query %s]: [job %s]: Storing answer %s",
+            query.uid,
+            job_id,
+            str(values),
+        )
         placeholders = ', '.join(['?' for _ in range(len(rows[0]))])
         columns = ', '.join(
             [f'`{qid}`' for qid in slots]
