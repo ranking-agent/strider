f6630b5f52965bd5f97e6341ef5c1e6963166639
Alon Greyber <alon@greyber.org>
2020-12-17 14:25:50 -0500

    Added simple test for ex1

--- tests/query_planner/test_query_planner.py
+++ tests/query_planner/test_query_planner.py
@@ -6,15 +6,32 @@ import pytest
 
 from tests.helpers.context import with_registry_overlay
 
+from strider.config import settings
+
+# Switch settings before importing strider things
+registry_host = "registry"
+settings.kpregistry_url = f"http://{registry_host}"
+
+from strider.query_planner import generate_plan
+
+
 cwd = Path(__file__).parent
+
 
 with open(cwd / "ex1_kps.json", "r") as f:
     kps = json.load(f)
 
-print(__file__)
-
 
 @pytest.mark.asyncio
-@with_registry_overlay("registry", kps)
-async def test_planner():
-    pass
+@with_registry_overlay(registry_host, kps)
+async def test_ex1():
+
+    with open(cwd / "ex1_qg.json", "r") as f:
+        qg = json.load(f)
+
+    plans = await generate_plan(qg)
+
+    assert plans
+
+    # We should have two valid plans
+    assert len(plans) == 2
