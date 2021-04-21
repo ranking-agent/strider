f665b5fc61f0445559d261571c260e2730a94309
Alon Greyber <alon@greyber.org>
2021-04-21 09:21:20 -0400

    Added tests for the filter_by_qgraph method

--- tests/trapi/test_trapi.py
+++ tests/trapi/test_trapi.py
@@ -1,7 +1,7 @@
 import pytest
 
 from strider.trapi import \
-    merge_messages
+    filter_by_qgraph, merge_messages
 
 
 @pytest.mark.asyncio
@@ -172,3 +172,139 @@ def test_merge_knowledge_graph_edges():
     edge = next(iter(edges.values()))
 
     assert edge["attributes"] == [ATTRIBUTE_A, ATTRIBUTE_B]
+
+
+def test_filter_by_qgraph_id():
+    """
+    Test that the filter_by_qgraph method
+    removes nodes with the wrong ID
+    """
+
+    message = {
+        "knowledge_graph" : {
+            "nodes" : {
+                "MONDO:1" : {},
+                "MONDO:2" : {}
+            },
+            "edges" : {}
+        },
+        "results" : [
+            {
+                "node_bindings" : {"n0" : [{"id" : "MONDO:1"}]},
+                "edge_bindings" : {},
+            },
+            {
+                "node_bindings" : {"n0" : [{"id" : "MONDO:2"}]},
+                "edge_bindings" : {},
+            },
+        ]
+    }
+
+    qgraph = {
+        "nodes" : {
+            "n0" : {"id" : ["MONDO:1"]},
+        }
+    }
+
+    filter_by_qgraph(message, qgraph)
+
+    assert len(message["results"]) == 1
+    assert len(message["knowledge_graph"]["nodes"]) == 1
+
+def test_filter_by_qgraph_category():
+    """
+    Test that the filter_by_qgraph method
+    removes nodes with the wrong category
+
+    The only time this should happen is with a misbehaving KP
+    that doesn't return results that match its own type
+    """
+
+    message = {
+        "knowledge_graph" : {
+            "nodes" : {
+                "MONDO:1" : {
+                    "category" : ["biolink:Disease"]
+                },
+                "MONDO:2" : {
+                    "category" : ["biolink:ChemicalSubstance"]
+                }
+            },
+            "edges" : {}
+        },
+        "results" : [
+            {
+                "node_bindings" : {"n0" : [{"id" : "MONDO:1"}]},
+                "edge_bindings" : {},
+            },
+            {
+                "node_bindings" : {"n0" : [{"id" : "MONDO:2"}]},
+                "edge_bindings" : {},
+            },
+        ]
+    }
+
+    qgraph = {
+        "nodes" : {
+            "n0" : {"category" : ["biolink:DiseaseOrPhenotypicFeature"]},
+        }
+    }
+
+    filter_by_qgraph(message, qgraph)
+
+    assert len(message["results"]) == 1
+    assert len(message["knowledge_graph"]["nodes"]) == 1
+
+
+def test_filter_by_qgraph_predicate():
+    """
+    Test that the filter_by_qgraph method
+    removes edges with the wrong predicate
+
+    The only time this should happen is with a misbehaving KP
+    that doesn't return results that match its own type
+    """
+
+    message = {
+        "knowledge_graph" : {
+            "nodes" : {"n0" : {}},
+            "edges" : {
+                "ke1" : {
+                    "subject" : "n0",
+                    "object" : "n0",
+                    "predicate" : "biolink:ameliorates",
+                },
+                "ke2" : {
+                    "subject" : "n0",
+                    "object" : "n0",
+                    "predicate" : "biolink:treats",
+                }
+            },
+        },
+        "results" : [
+            {
+                "node_bindings" : {},
+                "edge_bindings" : {"e1" : [{"id" : "ke1"}]},
+            },
+            {
+                "node_bindings" : {},
+                "edge_bindings" : {"e1" : [{"id" : "ke2"}]},
+            },
+        ]
+    }
+
+    qgraph = {
+        "nodes" : {
+            "n0" : {},
+        },
+        "edges" : {
+            "e1" : {
+                "predicate" : ["biolink:treats"]
+            }
+        },
+    }
+
+    filter_by_qgraph(message, qgraph)
+
+    assert len(message["results"]) == 1
+    assert len(message["knowledge_graph"]["edges"]) == 1
