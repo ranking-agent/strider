59091d751c1c160bdb8547bc427fe1f0012674f9
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-05-28 14:30:10 -0400

    Use default helper URLs that will work natively
    Docker Compose can override these as necessary.

--- strider/config.py
+++ strider/config.py
@@ -7,12 +7,12 @@ from pydantic import \
 
 class Settings(BaseSettings):
     openapi_server_url: Optional[AnyUrl]
-    kpregistry_url: AnyUrl = "http://kp-registry:4983"
+    kpregistry_url: AnyUrl = "http://kp-registry.renci.org"
     omnicorp_url: AnyUrl = "http://robokop.renci.org:3210"
     biolink_url: AnyUrl = "https://bl-lookup-sri.renci.org"
     normalizer_url: AnyUrl = "https://nodenormalization-sri.renci.org"
 
-    redis_url: RedisDsn = "redis://redis"
+    redis_url: RedisDsn = "redis://localhost"
     store_results_for: timedelta = timedelta(days=7)
 
     class Config:
