8c6748fdc9d876fe019dfb9c010155bb2d9ed587
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-06-10 14:19:30 -0400

    Fix default KP registry URL

--- strider/config.py
+++ strider/config.py
@@ -7,7 +7,7 @@ from pydantic import \
 
 class Settings(BaseSettings):
     openapi_server_url: Optional[AnyUrl]
-    kpregistry_url: AnyUrl = "http://kp-registry.renci.org"
+    kpregistry_url: AnyUrl = "https://kp-registry.renci.org"
     omnicorp_url: AnyUrl = "http://robokop.renci.org:3210"
     biolink_url: AnyUrl = "https://bl-lookup-sri.renci.org"
     normalizer_url: AnyUrl = "https://nodenormalization-sri.renci.org"
