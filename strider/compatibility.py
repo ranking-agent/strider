c9127210bc54ee1c9f7c981849f2f05e56d5972b
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-08-04 20:10:24 -0400

    Handle missing kedge attributes in KP response

--- strider/compatibility.py
+++ strider/compatibility.py
@@ -165,10 +165,10 @@ def add_source(message: Message):
     """Add provenance annotation to kedges.
        Sources from which we retrieve data add their own prov, we add prov for aragorn."""
     for kedge in message["knowledge_graph"]["edges"].values():
-        kedge["attributes"].append(dict(
+        kedge["attributes"] = kedge.get("attributes", []) + [dict(
             attribute_type_id="biolink:aggregator_knowledge_source",
             value="infores:aragorn",
-        ))
+        )]
 
 
 Entity = namedtuple("Entity", ["categories", "identifiers"])
