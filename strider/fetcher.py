44f3d8468a126b9529336102b0600f6490327aa9
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-08-12 17:34:15 -0400

    Overhaul - fully parallelize, without queue

--- strider/fetcher.py
+++ strider/fetcher.py
@@ -11,18 +11,21 @@ oo     .d8P   888 .  888      888  888   888  888    .o  888
 """
 import asyncio
 from collections.abc import Iterable
+import copy
 from itertools import chain
 from json.decoder import JSONDecodeError
 import logging
 from datetime import datetime
-from typing import Optional
+from typing import Any, Optional
 
+import aiostream
 import httpx
 from reasoner_pydantic import QueryGraph, Result, Response
 from redis import Redis
 from trapi_throttle.throttle import ThrottledServer
 
-from .query_planner import generate_plans, Step, NoAnswersError
+from .graph import Graph
+from .query_planner import generate_plans, Step, NoAnswersError, pick_kps
 from .compatibility import KnowledgePortal, Synonymizer
 from .trapi import canonicalize_qgraph, filter_by_qgraph, map_qgraph_curies, merge_messages, merge_results, \
     fill_categories_predicates
@@ -61,31 +64,18 @@ class ReasonerLogEntryFormatter(logging.Formatter):
         return log_entry
 
 
-class StriderWorker(Worker):
-    """Async worker to process query"""
+class Binder():
+    """Binder."""
 
     def __init__(
             self,
             qid: str,
-            qgraph: dict,
-            *args,
             log_level: int = logging.DEBUG,
+            name: Optional[str] = None,
             redis_client: Optional[Redis] = None,
-            **kwargs,
     ):
         """Initialize."""
-        self.plan: dict[Step, list]
-        self.preferred_prefixes: dict[str, list[str]]
-        self.qgraph: RedisGraph
-        self.kgraph: RedisGraph
-        self.logger: logging.Logger
-        self.results: list[Result] = []
-        self.portal: KnowledgePortal = None
-        super().__init__(*args, **kwargs)
-
-        # Set up DB results objects
-        self.kgraph = RedisGraph(f"{qid}:kgraph", redis_client)
-        self.results = RedisList(f"{qid}:results", redis_client)
+        self.name = name
 
         # Set up logger
         handler = RedisLogHandler(f"{qid}:log", redis_client)
@@ -95,24 +85,196 @@ class StriderWorker(Worker):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(log_level)
         self.logger.addHandler(handler)
-
         self.logger.debug("Initialized strider worker")
 
-        if not self.portal:
-            self.portal = KnowledgePortal(self.logger)
+        self.portal = KnowledgePortal(self.logger)
 
-        # Pull query graph from Redis
-        self.qgraph = qgraph
+        # Set up DB results objects
+        self.kgraph = RedisGraph(f"{qid}:kgraph", redis_client)
+        self.results = RedisList(f"{qid}:results", redis_client)
 
-        # Use BMT for preferred prefixes
         self.preferred_prefixes = WBMT.entity_prefix_mapping
+
+    async def lookup(
+            self,
+            qgraph: Graph = None,
+            use_cache: bool = True,
+    ):
+        """Expand from query graph node."""
+        # if this is a leaf node, we're done
+        if qgraph is None:
+            qgraph = Graph(self.qgraph)
+        if not qgraph["edges"]:
+            yield {"nodes": dict(), "edges": dict()}, {"node_bindings": dict(), "edge_bindings": dict()}
+            return
+        self.logger.debug(f"Lookup for qgraph: {qgraph}")
+
+        try:
+            qedge_id, qedge = next(
+                (qedge_id, qedge)
+                for qedge_id, qedge in qgraph["edges"].items()
+                if any(
+                    qnode.get("ids", [])
+                    for qnode in (
+                        qgraph["nodes"][qedge["subject"]],
+                        qgraph["nodes"][qedge["object"]],
+                    )
+                )
+            )
+        except StopIteration:
+            raise RuntimeError("Cannot find qedge with pinned endpoint")
+
+        qedge = qgraph["edges"][qedge_id]
+        onehop = {
+            "nodes": {
+                key: value
+                for key, value in qgraph["nodes"].items()
+                if key in (qedge["subject"], qedge["object"])
+            },
+            "edges": {
+                qedge_id: qedge
+            }
+        }
+
+        generators = [
+            self.generate_from_kp(qgraph, onehop, self.kps[kp_id], use_cache=use_cache)
+            for kp_id in self.plan[qedge_id]
+        ]
+        async with aiostream.stream.merge(*generators).stream() as streamer:
+            async for output in streamer:
+                yield output
+
+    async def generate_from_kp(
+        self,
+        qgraph,
+        onehop_qgraph,
+        kp: KnowledgeProvider,
+        **kwargs,
+    ):
+        """Generate one-hop results from KP."""
+        print("sending", onehop_qgraph)
+        onehop_response = await kp.solve_onehop(
+            onehop_qgraph,
+            # **kwargs,
+        )
+        onehop_kgraph = onehop_response["knowledge_graph"]
+        onehop_results = onehop_response["results"]
+        qedge_id = next(iter(qgraph["edges"].keys()))
+        generators = []
+        for result in onehop_results:
+            # add edge to results and kgraph
+            result_kgraph = {
+                "nodes": {
+                    binding["id"]: onehop_kgraph["nodes"][binding["id"]]
+                    for _, bindings in result["node_bindings"].items()
+                    for binding in bindings
+                },
+                "edges": {
+                    binding["id"]: onehop_kgraph["edges"][binding["id"]]
+                    for _, bindings in result["edge_bindings"].items()
+                    for binding in bindings
+                },
+            }
+
+            # now solve the smaller question
+            subqgraph = copy.deepcopy(qgraph)
+            # remove edge
+            subqgraph["edges"].pop(qedge_id)
+            # pin node
+            for qnode_id, bindings in result["node_bindings"].items():
+                subqgraph["nodes"][qnode_id]["ids"] = [
+                    binding["id"]
+                    for binding in bindings
+                ]
+            # remove orphaned nodes
+            subqgraph.remove_orphaned()
+
+            generators.append(self.generate_from_result(subqgraph, result_kgraph, result))
+
+        async with aiostream.stream.merge(*generators).stream() as streamer:
+            async for result in streamer:
+                yield result
+
+    async def generate_from_result(
+        self,
+        qgraph,
+        kgraph,
+        result,
+        **kwargs,
+    ):
+        # LOGGER.debug(
+        #     "Expanding from result %s...",
+        #     result,
+        # )
+        async for subkgraph, subresult in self.lookup(
+            qgraph,
+            **kwargs,
+        ):
+            # combine one-hop with subquery results
+            subresult = {
+                "node_bindings": {
+                    **subresult["node_bindings"],
+                    **result["node_bindings"],
+                },
+                "edge_bindings": {
+                    **subresult["edge_bindings"],
+                    **result["edge_bindings"],
+                },
+            }
+            subkgraph["nodes"].update(kgraph["nodes"])
+            subkgraph["edges"].update(kgraph["edges"])
+            yield subkgraph, subresult
+
+    async def get_results(
+        self,
+        qgraph: dict[str, Any],
+        use_cache=True,
+        max_results=None,
+    ):
+        """Get results and kgraph."""
+        qgraph = copy.deepcopy(qgraph)
+        qgraph = Graph(qgraph)
+        # normalize_qgraph(qgraph)
+
+        kgraph = {"nodes": dict(), "edges": dict()}
+        results = []
+        counter = 0
+        async for kgraph_, result_ in self.lookup(qgraph, use_cache=use_cache):
+            # aggregate results
+            kgraph["nodes"].update(kgraph_["nodes"])
+            kgraph["edges"].update(kgraph_["edges"])
+            results.append(result_)
+            counter += 1
+            if max_results is not None and counter >= max_results:
+                break
+
+        for kedge in kgraph["edges"].values():
+            kedge["attributes"] = [{
+                "attribute_type_id": "biolink:knowledge_source",
+                "value": f"infores:{self.name}",
+            }]
+
+        return kgraph, results
+
+    async def __aenter__(self):
+        """Enter context."""
+        for tserver in self.portal.tservers.values():
+            await tserver.__aenter__()
+        return self
+
+    async def __aexit__(self, *args):
+        """Exit context."""
+        for tserver in self.portal.tservers.values():
+            await tserver.__aexit__()
 
     # pylint: disable=arguments-differ
     async def setup(
             self,
+            qgraph: dict,
     ):
         """Set up."""
         # Update qgraph identifiers
+        self.qgraph = qgraph
         synonymizer = Synonymizer(self.logger)
         await synonymizer.load_message({"query_graph": self.qgraph})
         curie_map = synonymizer.map(self.preferred_prefixes)
@@ -130,206 +292,65 @@ class StriderWorker(Worker):
             if edge.get("constraints"):
                 raise ValueError("Unable to process query due to constraints")
 
-    async def generate_plan(self):
-        """
-        Use the self.qgraph object to generate a plan and store it
-        in the self.plan object.
-
-        Also adds a partial result to the queue so that the
-        run method can be called.
-        """
-
         # Initialize registry
         registry = Registry(settings.kpregistry_url, self.logger)
 
         self.logger.debug("Generating plan")
         # Generate traversal plan
-        plans, kps = await generate_plans(
+        self.plan, kps = await pick_kps(
             self.qgraph,
             kp_registry=registry,
-            logger=self.logger)
-
-        if len(plans) == 0:
-            self.logger.error("Could not find a plan to traverse query graph")
-            raise NoAnswersError()
-
-        self.plan = plans[0]
+            logger=self.logger,
+        )
 
         # extract KP preferred prefixes from plan
         self.kp_preferred_prefixes = dict()
         self.portal.tservers = dict()
-        for _kps in kps.values():
-            for kp in _kps:
-                url = kp["url"][:-5] + "meta_knowledge_graph"
-                try:
-                    async with httpx.AsyncClient() as client:
-                        response = await client.get(
-                            url,
-                            timeout=10,
-                        )
-                    meta_kg = response.json()
-                    self.kp_preferred_prefixes[kp["id"]] = {
-                        category: data["id_prefixes"]
-                        for category, data in meta_kg["nodes"].items()
-                    }
-                except (httpx.ConnectError, httpx.ConnectTimeout, httpx.ReadTimeout) as err:
-                    self.logger.warning(
-                        "Unable to get meta knowledge graph from KP {}: {}".format(
-                            kp["id"],
-                            str(err),
-                        ),
+        for kp_id, kp in kps.items():
+            url = kp["url"][:-5] + "meta_knowledge_graph"
+            try:
+                async with httpx.AsyncClient() as client:
+                    response = await client.get(
+                        url,
+                        timeout=10,
                     )
-                    self.kp_preferred_prefixes[kp["id"]] = dict()
-                except JSONDecodeError as err:
-                    self.logger.warning(
-                        "Unable to parse meta knowledge graph from KP {}: {}".format(
-                            kp["id"],
-                            str(err),
-                        ),
-                    )
-                    self.kp_preferred_prefixes[kp["id"]] = dict()
-                self.portal.tservers[kp["id"]] = ThrottledServer(
-                    kp["id"],
-                    url=kp["url"],
-                    request_qty=1,
-                    request_duration=1,
+                meta_kg = response.json()
+                self.kp_preferred_prefixes[kp_id] = {
+                    category: data["id_prefixes"]
+                    for category, data in meta_kg["nodes"].items()
+                }
+            except (httpx.ConnectError, httpx.ConnectTimeout, httpx.ReadTimeout) as err:
+                self.logger.warning(
+                    "Unable to get meta knowledge graph from KP {}: {}".format(
+                        kp_id,
+                        str(err),
+                    ),
                 )
-        self.kp_details = {
-            step: [
-                KnowledgeProvider(
-                    details,
-                    self.portal,
-                    details["id"],
-                    self.kp_preferred_prefixes[details["id"]],
-                    self.preferred_prefixes,
+                self.kp_preferred_prefixes[kp_id] = dict()
+            except JSONDecodeError as err:
+                self.logger.warning(
+                    "Unable to parse meta knowledge graph from KP {}: {}".format(
+                        kp["id"],
+                        str(err),
+                    ),
                 )
-                for details in options
-            ]
-            for step, options in kps.items()
-        }
-
-        self.logger.debug({
-            "plan": self.plan,
-        })
-
-        # Initialize results
-
-        first_qedge_id = self.plan[0]
-        first_edge = self.qgraph["edges"][first_qedge_id]
-        first_node_id, first_node = next(
-            (node_id, node)
-            for node_id in (
-                first_edge["subject"],
-                first_edge["object"],
+                self.kp_preferred_prefixes[kp_id] = dict()
+            self.portal.tservers[kp_id] = ThrottledServer(
+                kp_id,
+                url=kp["url"],
+                request_qty=1,
+                request_duration=1,
             )
-            if (node := self.qgraph["nodes"][node_id]).get("ids", None)
-        )
-
-        # Remove ID from pinned node because we will
-        # add it back manually later in the get_kp_request_body
-        # function
-        curies = first_node.pop("ids")
-
-        # Add partial results for each curie we are given
-        for curie in curies:
-            result = {
-                "node_bindings": {
-                    first_node_id: [{
-                        "id": curie,
-                    }]
-                },
-                "edge_bindings": {},
-            }
-            await self.put(result)
-
-    async def __aenter__(self):
-        """Enter context."""
-        for tserver in self.portal.tservers.values():
-            await tserver.__aenter__()
-        return self
-
-    async def __aexit__(self, *args):
-        """Exit context."""
-        for tserver in self.portal.tservers.values():
-            await tserver.__aexit__()
-
-
-    def next_step(
-            self,
-            bound_edges: Iterable[str],
-    ):
-        """Get next step in plan."""
-        return next(
-            qedge_id
-            for qedge_id in self.plan
-            if qedge_id not in bound_edges
-        )
-
-    @ async_locking_cache
-    async def execute_step(
-            self,
-            step: Step,
-            node_bindings: tuple[tuple[str]],
-    ):
-        """Fetch results for step."""
-
-        self.logger.debug({
-            "description": "Executing step: ",
-            "step": step,
-        })
-
-        # For each KP used in this step, make the appropriate request and inverse-predicate request
-        qgraph = get_kp_request_qgraph(
-            self.qgraph,
-            node_bindings,
-            step,
-        )
-        responses = await asyncio.gather(*(
-            kp.solve_onehop(qgraph)
-            for kp in self.kp_details[step]
-        ))
-
-        for response in responses:
-            filter_by_qgraph(response, self.qgraph)
-
-        return merge_messages(responses)
-
-    async def on_message(
-            self,
-            result: Result,
-    ):
-        """Process partial result."""
-        # find the next step in the plan
-        try:
-            qedge_id = self.next_step(result["edge_bindings"])
-        except StopIteration:
-            # Mission accomplished!
-            self.results.append(result)
-            return
-
-        # execute step
-        self.logger.debug({
-            "description": "Recieved results from KPs",
-            "data": result,
-            "step": qedge_id,
-        })
-
-        response = await self.execute_step(
-            qedge_id,
-            tuple(
-                (qnode_id, bindings[0]["id"])
-                for qnode_id, bindings in result["node_bindings"].items()
-            ),
-        )
-
-        # process kgraph
-        self.kgraph.nodes.merge(response["knowledge_graph"]["nodes"])
-        self.kgraph.edges.merge(response["knowledge_graph"]["edges"])
-
-        # process results
-        for new_result in response["results"]:
-            # queue the results for further processing
-            await self.put(merge_results([result, new_result]))
+        self.kps = {
+            kp_id: KnowledgeProvider(
+                details,
+                self.portal,
+                kp_id,
+                self.kp_preferred_prefixes[kp_id],
+                self.preferred_prefixes,
+            )
+            for kp_id, details in kps.items()
+        }
 
 
 def get_kp_request_qgraph(
