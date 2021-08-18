d737c454feea58c5611a1e87e7eb10da7560b687
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-08-18 14:38:33 -0400

    Add constraints utilities

---
+++ strider/constraints.py
@@ -0,0 +1,74 @@
+"""Constraint handling."""
+import operator
+import re
+
+operator_map = {
+    "==": operator.eq,
+    ">": operator.gt,
+    "<": operator.lt,
+    "matches": lambda s, pattern: re.fullmatch(pattern, s) is not None,
+}
+
+
+def satisfies_constraint(kel: dict, constraint: dict) -> bool:
+    """Determine whether knowledge graph element satisfies constraint.
+
+    If the constrained attribute is missing, returns False.
+    """
+    try:
+        attribute = next(
+            attribute
+            for attribute in kel.get("attributes", None) or []
+            if attribute["attribute_type_id"] == constraint["id"]
+        )
+    except StopIteration:
+        return False
+    return constraint.get("not", False) != operator_map[constraint["operator"]](
+        attribute["value"],
+        constraint["value"],
+    )
+
+
+def result_satisfies_constraints(result: dict, kgraph: dict, qgraph: dict) -> bool:
+    """Determine whether result satisfies qgraph constraints."""
+    for qnode_id, node_bindings in result["node_bindings"].items():
+        for node_binding in node_bindings:
+            knode = kgraph["nodes"][node_binding["id"]]
+            for constraint in qgraph["nodes"][qnode_id].get("constraints", []):
+                if not satisfies_constraint(knode, constraint):
+                    return False
+    for qedge_id, edge_bindings in result["edge_bindings"].items():
+        for edge_binding in edge_bindings:
+            kedge = kgraph["edges"][edge_binding["id"]]
+            for constraint in qgraph["edges"][qedge_id].get("constraints", []):
+                if not satisfies_constraint(kedge, constraint):
+                    return False
+    return True
+
+
+def enforce_constraints(message: dict) -> dict:
+    """Enforce qgraph constraints from qgraph on results/kgraph."""
+    message["results"] = [
+        result
+        for result in message["results"]
+        if result_satisfies_constraints(
+            result,
+            message["knowledge_graph"],
+            message["query_graph"],
+        )
+    ]
+    message["knowledge_graph"] = {
+        "nodes": {
+            binding["id"]: message["knowledge_graph"]["nodes"][binding["id"]]
+            for result in message["results"]
+            for _, bindings in result["node_bindings"].items()
+            for binding in bindings
+        },
+        "edges": {
+            binding["id"]: message["knowledge_graph"]["edges"][binding["id"]]
+            for result in message["results"]
+            for _, bindings in result["edge_bindings"].items()
+            for binding in bindings
+        },
+    }
+    return message
---
+++ tests/test_constraints.py
@@ -0,0 +1,169 @@
+"""Test constraints."""
+from strider.constraints import satisfies_constraint, enforce_constraints
+
+
+def test_equal():
+    """Test satisfies_constraint() with operator "=="."""
+    qnode = {
+        "attributes": [
+            {
+                "attribute_type_id": "test",
+                "value": "foo",
+            },
+        ],
+    }
+    constraint = {
+        "id": "test",
+        "operator": "==",
+        "value": "foo",
+    }
+    assert satisfies_constraint(qnode, constraint)
+    constraint = {
+        "id": "test",
+        "operator": "==",
+        "value": "bar",
+    }
+    assert not satisfies_constraint(qnode, constraint)
+    constraint = {
+        "id": "test",
+        "not": True,
+        "operator": "==",
+        "value": "bar",
+    }
+    assert satisfies_constraint(qnode, constraint)
+
+
+def test_gt():
+    """Test satisfies_constraint() with opertor ">"."""
+    qnode = {
+        "attributes": [
+            {
+                "attribute_type_id": "test",
+                "value": 5,
+            },
+        ],
+    }
+    constraint = {
+        "id": "test",
+        "operator": ">",
+        "value": 3,
+    }
+    assert satisfies_constraint(qnode, constraint)
+    constraint = {
+        "id": "test",
+        "operator": ">",
+        "value": 7,
+    }
+    assert not satisfies_constraint(qnode, constraint)
+    constraint = {
+        "id": "test",
+        "not": True,
+        "operator": ">",
+        "value": 7,
+    }
+    assert satisfies_constraint(qnode, constraint)
+
+
+def test_match():
+    """Test satisfies_constraint() with opertor "matches"."""
+    qnode = {
+        "attributes": [
+            {
+                "attribute_type_id": "test",
+                "value": "Mississippi",
+            },
+        ],
+    }
+    constraint = {
+        "id": "test",
+        "operator": "matches",
+        "value": r".*(iss){2,}.*",
+    }
+    assert satisfies_constraint(qnode, constraint)
+    constraint = {
+        "id": "test",
+        "operator": "matches",
+        "value": r".*(iss){3,}.*",
+    }
+    assert not satisfies_constraint(qnode, constraint)
+    constraint = {
+        "id": "test",
+        "not": True,
+        "operator": "matches",
+        "value": r".*(iss){3,}.*",
+    }
+    assert satisfies_constraint(qnode, constraint)
+
+
+def test_enforce_constraints():
+    """Test enforce_constraints()."""
+    message = {
+        "query_graph": {
+            "nodes": {},
+            "edges": {},
+        },
+        "knowledge_graph": {
+            "nodes": {},
+            "edges": {},
+        },
+        "results": [],
+    }
+    assert message == enforce_constraints(message)
+    message = {
+        "query_graph": {
+            "nodes": {
+                "n0": {
+                    "constraints": [
+                        {
+                            "id": "test",
+                            "operator": "==",
+                            "value": "foo",
+                        },
+                    ],
+                },
+            },
+            "edges": {},
+        },
+        "knowledge_graph": {
+            "nodes": {
+                "a": {
+                    "attributes": [
+                        {
+                            "attribute_type_id": "test",
+                            "value": "foo",
+                        },
+                    ],
+                },
+                "b": {
+                    "attributes": [
+                        {
+                            "attribute_type_id": "test",
+                            "value": "bar",
+                        },
+                    ],
+                },
+            },
+            "edges": {},
+        },
+        "results": [
+            {
+                "node_bindings": {
+                    "n0": [
+                        {"id": "a"},
+                    ],
+                },
+                "edge_bindings": {},
+            },
+            {
+                "node_bindings": {
+                    "n0": [
+                        {"id": "b"},
+                    ],
+                },
+                "edge_bindings": {},
+            },
+        ],
+    }
+    constrained = enforce_constraints(message)
+    assert len(constrained["results"]) == 1
+    assert list(constrained["knowledge_graph"]["nodes"].keys()) == ["a"]
