be2a59d43ea2d6f5b37377659aa28b0ec17d6da7
Alon Greyber <alon@greyber.org>
2021-09-10 16:33:56 -0400

    Changed /profile output to JSON

--- strider/profiler.py
+++ strider/profiler.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import time
+import uuid
 
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
@@ -7,9 +8,21 @@ import yappi
 
 from .server import APP
 
+class DownloadStaticFiles(StaticFiles):
+    """
+    Wrapper on StaticFiles to serve files that are automatically
+    downloaded using the Content-Disposition header
+    """
+    def file_response(*args, **kwargs):
+        resp = StaticFiles.file_response(*args, **kwargs)
+        resp.headers["Content-Disposition"] = "attachment"
+        return resp
+
 # Create directory to store request profile data
 PROFILE_DIRECTORY = "/profiles"
 Path(PROFILE_DIRECTORY).mkdir(exist_ok = True)
+
+captured_profiles = []
 
 # Middleware to capture profile data and save it
 # to files
@@ -19,38 +32,34 @@ async def profiler_middleware(request, call_next):
     with yappi.run():
         call_result = await call_next(request)
 
+    # Generate ID for this request
+    request_id = str(uuid.uuid1())
+
     # Save profile
     stats = yappi.get_func_stats()
-    stats.save(f"{PROFILE_DIRECTORY}/req-{time.time()}.prof", type='pstat')
+    stats.save(f"{PROFILE_DIRECTORY}/{request_id}.prof", type='pstat')
+
+    download_link = \
+        f"{request.url.scheme}://{request.url.netloc}/profiles/{request_id}.prof"
+
+    # Save profile meta-info
+    captured_profiles.append({
+        "id" : request_id,
+        "timestamp" : time.time(),
+        "path" : request.url.path,
+        "download_link" : download_link,
+    })
 
     # Continue
     return call_result
 
 # Serve prof files under /profiles/
 APP.mount("/profiles",
-          StaticFiles(directory=PROFILE_DIRECTORY),
+          DownloadStaticFiles(directory=PROFILE_DIRECTORY),
           name="profiles",
           )
 
-@APP.get("/profiles", response_class=HTMLResponse)
+@APP.get("/profiles")
 async def profiles_list():
-    """ Helper page that lists available profiles to download """
-
-    profile_links = [
-        f"<li><a href='/profiles/{p.name}' download>{p.stem}</a></li>"
-        for p in Path(PROFILE_DIRECTORY).iterdir()
-    ]
-
-    return f"""
-    <html>
-        <head>
-            <title>Strider Profiles</title>
-        </head>
-        <body>
-            <h1>Available request profiles for download:</h1>
-            <ul>
-                {"".join(profile_links)}
-            </ul>
-        </body>
-    </html>
-    """
+    """ Get all profiles captured in the current session"""
+    return captured_profiles
