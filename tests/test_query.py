bb2e303374a4041ee323df0c509cbaa1bc9d3ed4
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-09-20 21:33:49 -0400

    Add qnode_id binding test

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,8 +1,11 @@
 """Test Strider."""
+import json
+
 import fakeredis
+from fastapi.responses import Response
 import pytest
 
-from tests.helpers.context import with_translator_overlay
+from tests.helpers.context import with_translator_overlay, with_response_overlay
 from tests.helpers.logger import setup_logger
 from tests.helpers.utils import query_graph_from_string, validate_message
 
@@ -157,3 +160,114 @@ async def test_symmetric_noncanonical(redis):
     output = await lookup(q, redis)
 
     assert len(output["message"]["results"]) == 1
+
+
+@pytest.mark.asyncio
+@with_translator_overlay(
+    settings.kpregistry_url,
+    settings.normalizer_url,
+    {
+        "ctd":
+        """
+            CHEBI:6801(( category biolink:SmallMolecule ))
+            MONDO:0005148(( category biolink:Disease ))
+            CHEBI:6801-- predicate biolink:treats -->MONDO:0005148
+        """,
+    }
+)
+# Add attributes to ctd response
+@with_response_overlay(
+    "http://ctd/query",
+    Response(
+        status_code=200,
+        content=json.dumps({"message": {
+            "query_graph": {
+                "nodes": {
+                    "n0": {"ids": ["CHEBI:6801"]},
+                    "n1": {"categories": ["biolink:Disease"]},
+                },
+                "edges": {
+                    "n0n1": {
+                        "subject": "n0",
+                        "predicate": "biolink:treats",
+                        "object": "n1",
+                    },
+                },
+            },
+            "knowledge_graph": {
+                "nodes": {
+                    "CHEBI:XXX": {},
+                    "MONDO:0005148": {
+                        "attributes": [
+                            {
+                                "attribute_type_id": "test_constraint",
+                                "value": "foo",
+                            },
+                        ],
+                    },
+                },
+                "edges": {
+                    "n0n1": {
+                        "subject": "CHEBI:XXX",
+                        "predicate": "biolink:treats",
+                        "object": "MONDO:0005148",
+                    },
+                },
+            },
+            "results": [
+                {
+                    "node_bindings": {
+                        "n0": [{
+                            "id": "CHEBI:XXX",
+                            "qnode_id": "CHEBI:6801",
+                        }],
+                        "n1": [{"id": "MONDO:0005148"}],
+                    },
+                    "edge_bindings": {
+                        "n0n1": [{"id": "n0n1"}],
+                    },
+                },
+            ],
+        }}),
+    )
+)
+async def test_disambiguation(redis):
+    """
+    Test disambiguating batch results with qnode_id.
+    """
+    QGRAPH = query_graph_from_string(
+        """
+        n0(( ids[] CHEBI:6801 ))
+        n0-- biolink:treats -->n1
+        n1(( categories[] biolink:Disease ))
+        """
+    )
+
+    # Create query
+    q = {
+        "message" : {"query_graph" : QGRAPH},
+        "log_level": "ERROR",
+    }
+
+    # Run
+    output = await lookup(q, redis)
+    assert len(output["message"]["results"]) == 1
+
+    validate_message(
+        {
+            "knowledge_graph":
+                """
+                CHEBI:XXX biolink:treats MONDO:0005148
+                """,
+            "results": [
+                """
+                node_bindings:
+                    n0 CHEBI:XXX
+                    n1 MONDO:0005148
+                edge_bindings:
+                    n0n1 CHEBI:XXX-MONDO:0005148
+                """,
+            ],
+        },
+        output["message"]
+    )
