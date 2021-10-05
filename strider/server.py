0710565c9c602ac67201bc3002b821a80ec1d83b
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-10-05 16:37:10 -0400

    Bump version

--- strider/server.py
+++ strider/server.py
@@ -42,7 +42,7 @@ openapi_args = dict(
     title="Strider",
     description=DESCRIPTION,
     docs_url=None,
-    version="3.13.0",
+    version="3.13.1",
     terms_of_service=(
         "http://robokop.renci.org:7055/tos"
         "?service_long=Strider"
