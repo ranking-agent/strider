43c29aebf500953200afd83afb9278d13609bb03
Max Wang <max@covar.com>
2023-05-26 13:51:57 -0400

    Add num results to cache hit log message

--- strider/fetcher.py
+++ strider/fetcher.py
@@ -107,7 +107,9 @@ class Binder:
         self.logger.info(f"Current call stack: {(', ').join(call_stack)}")
         onehop_response = await get_kp_onehop(kp.id, onehop_qgraph)
         if onehop_response is not None:
-            self.logger.info(f"[{kp.id}]: Got onehop from cache")
+            self.logger.info(
+                f"[{kp.id}]: Got onehop with {len(onehop_response['results'])} results from cache"
+            )
         if onehop_response is None and not settings.offline_mode:
             # onehop not in cache, have to go get response
             self.logger.info(
