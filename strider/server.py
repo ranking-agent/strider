97b2b34d96c852ba847ee18810ccdde77ebb0cac
Max Wang <max@covar.com>
2023-06-26 14:58:55 -0400

    Bump minor version

--- strider/server.py
+++ strider/server.py
@@ -64,7 +64,7 @@ openapi_args = dict(
     title="Strider",
     description=DESCRIPTION,
     docs_url=None,
-    version="4.3.4",
+    version="4.4.0",
     terms_of_service=(
         "http://robokop.renci.org:7055/tos"
         "?service_long=Strider"
