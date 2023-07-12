c93523861510760fc4f98858d2e0a819a752e462
Max Wang <max@covar.com>
2023-07-12 14:56:22 -0400

    Add regression test

--- tests/helpers/mock_responses.py
+++ tests/helpers/mock_responses.py
@@ -389,3 +389,143 @@ response_with_attributes = {
         ],
     },
 }
+
+response_with_aux_graphs = {
+    "message": {
+        "query_graph": {
+            "nodes": {
+                "n0": {"ids": ["MESH:D008687"]},
+                "n1": {"categories": ["biolink:Disease"]},
+            },
+            "edges": {
+                "n0n1": {
+                    "subject": "n0",
+                    "object": "n1",
+                    "predicates": ["biolink:treats"],
+                }
+            },
+        },
+        "knowledge_graph": {
+            "nodes": {
+                "MESH:D008687": {
+                    "categories": ["biolink:SmallMolecule"],
+                    "name": "Metformin",
+                },
+                "MONDO:0005148": {
+                    "categories": [
+                        "biolink:Disease",
+                    ],
+                    "name": "type 2 diabetes mellitus",
+                },
+                "MESH:D014867": {
+                    "categories": [
+                        "biolink:ChemicalEntity",
+                    ],
+                    "name": "Water",
+                },
+            },
+            "edges": {
+                "n0n1": {
+                    "subject": "MESH:D008687",
+                    "object": "MONDO:0005148",
+                    "predicate": "biolink:treats",
+                    "attributes": [
+                        {
+                            "value": "infores:kp3",
+                            "attribute_type_id": "biolink:knowledge_source",
+                        },
+                        {
+                            "attribute_type_id": "biolink:support_graphs",
+                            "value": [
+                                "2",
+                            ],
+                        },
+                    ],
+                    "sources": [
+                        {
+                            "resource_id": "infores:kp3",
+                            "resource_role": "primary_knowledge_source",
+                        },
+                    ],
+                },
+                "extra_edge_1": {
+                    "subject": "MESH:D014867",
+                    "object": "MONDO:0005148",
+                    "predicate": "biolink:treats",
+                    "attributes": [
+                        {
+                            "value": "infores:kp3",
+                            "attribute_type_id": "biolink:knowledge_source",
+                        },
+                    ],
+                    "sources": [
+                        {
+                            "resource_id": "infores:kp3",
+                            "resource_role": "primary_knowledge_source",
+                        },
+                    ],
+                },
+                "extra_edge_2": {
+                    "subject": "MESH:D014867",
+                    "object": "MONDO:0005148",
+                    "predicate": "biolink:ameliorates",
+                    "attributes": [
+                        {
+                            "value": "infores:kp3",
+                            "attribute_type_id": "biolink:knowledge_source",
+                        },
+                    ],
+                    "sources": [
+                        {
+                            "resource_id": "infores:kp3",
+                            "resource_role": "primary_knowledge_source",
+                        },
+                    ],
+                },
+            },
+        },
+        "results": [
+            {
+                "node_bindings": {
+                    "n0": [
+                        {
+                            "id": "MESH:D008687",
+                        },
+                    ],
+                    "n1": [
+                        {
+                            "id": "MONDO:0005148",
+                        },
+                    ],
+                },
+                "analyses": [
+                    {
+                        "resource_id": "kp3",
+                        "edge_bindings": {
+                            "n0n1": [
+                                {
+                                    "id": "n0n1",
+                                },
+                            ],
+                        },
+                        "support_graphs": [
+                            "1",
+                        ],
+                    },
+                ],
+            },
+        ],
+        "auxiliary_graphs": {
+            "1": {
+                "edges": [
+                    "extra_edge_1",
+                ],
+            },
+            "2": {
+                "edges": [
+                    "extra_edge_2",
+                ],
+            },
+        },
+    },
+}
--- tests/test_postproc.py
+++ tests/test_postproc.py
@@ -5,7 +5,7 @@ from reasoner_pydantic import Response
 from tests.helpers.context import (
     with_norm_overlay,
 )
-from tests.helpers.mock_responses import kp_response
+from tests.helpers.mock_responses import kp_response, response_with_aux_graphs
 
 from strider.config import settings
 
@@ -54,3 +54,43 @@ async def test_node_filtered():
     # MONDO:0005148 should be removed
     assert "MONDO:0005148" not in msg.message.knowledge_graph.nodes
     assert len(msg.message.results) == 0
+
+
+@pytest.mark.asyncio
+@with_norm_overlay(
+    settings.normalizer_url,
+    """
+    MONDO:0005148 categories biolink:Disease
+    MONDO:0005148 synonyms DOID:9352
+    MONDO:0005148 information_content 100
+    MESH:D008687 categories biolink:ChemicalEntity
+    MESH:D008687 synonyms PUBCHEM.COMPOUND:4901
+    MESH:D008687 information_content 100
+""",
+)
+async def test_aux_graph_filtering():
+    """
+    Test that node with an information content lower than the threshold are removed
+    """
+    provider = KnowledgeProvider("test", kp, logger)
+
+    preferred_prefixes = {"biolink:Disease": ["MONDO"]}
+
+    msg = Response.parse_obj(response_with_aux_graphs)
+
+    msg.message.auxiliary_graphs["test"] = {
+        "edges": [
+            "test_edge",
+        ],
+    }
+
+    assert len(list(msg.message.auxiliary_graphs.keys())) == 3
+    assert len(msg.message.results) == 1
+
+    processor = provider.get_postprocessor(preferred_prefixes)
+
+    await processor(msg, False)
+
+    # test aux graph should be removed
+    assert len(list(msg.message.auxiliary_graphs.keys())) == 2
+    assert len(msg.message.results) == 1
