096944f9e329c572d64adb9c203d493552e71e3b
Max Wang <max@covar.com>
2024-08-21 14:01:48 -0400

    Bump patch version

--- strider/server.py
+++ strider/server.py
@@ -74,7 +74,7 @@ openapi_args = dict(
     title="Strider",
     description=DESCRIPTION,
     docs_url=None,
-    version="4.8.0",
+    version="4.8.1",
     terms_of_service=(
         "http://robokop.renci.org:7055/tos"
         "?service_long=Strider"
