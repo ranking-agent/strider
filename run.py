fe5375ed927321e5053ac9f7d0acccbc9cd21bc1
Max Wang <max@covar.com>
2024-09-20 10:25:36 -0400

    Only reload when strider code changes

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("strider.server:APP", host="0.0.0.0", port=5781, reload=True)
+    uvicorn.run(
+        "strider.server:APP",
+        host="0.0.0.0",
+        port=5781,
+        reload=True,
+        reload_dirs=["strider"],
+    )
