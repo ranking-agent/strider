9e24f43288e3055df9d9fa8550e1532b052f471a
uhbrar <abrar.mesbah@gmail.com>
2025-08-01 21:20:09 -0400

    add recursive filtering

--- strider/trapi.py
+++ strider/trapi.py
@@ -244,6 +244,34 @@ def filter_ancestor_types(categories):
     return [category for category, drop in zip(categories, has_descendant) if not drop]
 
 
+def recursive_filter_edge_support_graphs(edge, filtered_edges, filtered_auxgraphs, kgraph, auxgraphs, filtered_nodes, logger):
+    """Recursive method to find auxiliary graphs to keep when filtering. Each auxiliary
+    graph then has its edges filterd."""
+    filtered_edges.add(edge)
+    filtered_nodes.add(kgraph.edges[edge].subject)
+    filtered_nodes.add(kgraph.edges[edge].object)
+    for attribute in kgraph.edges[edge].attributes:
+        if attribute.attribute_type_id == 'biolink:support_graphs':
+            for auxgraph in attribute.value:
+                if auxgraph not in auxgraphs:
+                    logger.warning(f"aux_edge {auxgraph} not in message.auxiliary_graphs")
+                    continue
+                filtered_edges, filtered_auxgraphs, filtered_nodes = recursive_filter_auxgraph_edges(auxgraph, filtered_edges, filtered_auxgraphs, kgraph, auxgraphs, filtered_nodes, logger)
+    return filtered_edges, filtered_auxgraphs, filtered_nodes
+
+
+def recursive_filter_auxgraph_edges(auxgraph, filtered_edges, filtered_auxgraphs, kgraph, auxgraphs, filtered_nodes, logger):
+    """Recursive method to find edges to keep when filtering. Each edge then
+    has support graphs filtered."""
+    filtered_auxgraphs.add(auxgraph)
+    for aux_edge in auxgraphs[auxgraph].edges:
+        if aux_edge not in kgraph.edges:
+            logger.warning(f"aux_edge {aux_edge} not in knowledge_graph.edges")
+            continue
+        filtered_edges, filtered_auxgraphs, filtered_nodes = recursive_filter_edge_support_graphs(aux_edge, filtered_edges, filtered_auxgraphs, kgraph, auxgraphs, filtered_nodes, logger)
+    return filtered_edges, filtered_auxgraphs, filtered_nodes
+
+
 def filter_message(
     message: Message,
     curie_map: dict,
@@ -325,43 +353,41 @@ def filter_message(
                         message.knowledge_graph.nodes.pop(node_binding.id)
         if keep:
             try:
+                filtered_nodes, filtered_edges, filtered_auxgraphs = set(), set(), set()
                 for analysis in result.analyses or []:
                     # add support graphs from result
                     for support_graph_id in analysis.support_graphs or []:
-                        kept_aux_graphs[support_graph_id] = message.auxiliary_graphs[
-                            support_graph_id
-                        ]
-                        for edge_id in message.auxiliary_graphs[support_graph_id].edges:
-                            kept_knowledge_graph.edges[edge_id] = (
-                                message.knowledge_graph.edges[edge_id]
-                            )
+                        filtered_edges, filtered_auxgraphs, filtered_nodes = recursive_filter_auxgraph_edges(
+                            support_graph_id,
+                            filtered_edges,
+                            filtered_auxgraphs,
+                            message.knowledge_graph,
+                            message.auxiliary_graphs,
+                            filtered_nodes,
+                            logger
+                        )
                     # add edges from result
                     for edge_bindings in analysis.edge_bindings.values():
                         for edge_binding in edge_bindings:
-                            kept_knowledge_graph.edges[edge_binding.id] = (
-                                message.knowledge_graph.edges[edge_binding.id]
+                            filtered_edges, filtered_auxgraphs, filtered_nodes = recursive_filter_edge_support_graphs(
+                                edge_binding.id,
+                                filtered_edges,
+                                filtered_auxgraphs,
+                                message.knowledge_graph,
+                                message.auxiliary_graphs,
+                                filtered_nodes,
+                                logger
                             )
-                            # add support graphs from edge
-                            for attribute in (
-                                kept_knowledge_graph.edges[edge_binding.id].attributes
-                                or []
-                            ):
-                                if (
-                                    attribute.attribute_type_id
-                                    == "biolink:support_graphs"
-                                ):
-                                    for aux_graph_id in attribute.value:
-                                        kept_aux_graphs[aux_graph_id] = (
-                                            message.auxiliary_graphs[aux_graph_id]
-                                        )
-                                        for edge_id in message.auxiliary_graphs[
-                                            aux_graph_id
-                                        ].edges:
-                                            kept_knowledge_graph.edges[edge_id] = (
-                                                message.knowledge_graph.edges[edge_id]
-                                            )
                 # keep any results that don't have promiscuous nodes
                 # only add result if all the knowledge and aux graph stuff worked out
+                for support_graph_id in filtered_auxgraphs:
+                    kept_aux_graphs[support_graph_id] = message.auxiliary_graphs[
+                        support_graph_id
+                    ]
+                for edge_id in filtered_edges:
+                    kept_knowledge_graph.edges[edge_id] = (
+                        message.knowledge_graph.edges[edge_id]
+                    )
                 kept_results.append(result)
             except Exception as e:
                 logger.error(f"Error while filtering message: {str(e)}")
