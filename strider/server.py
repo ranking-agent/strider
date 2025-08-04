1112b69824bcb8641eca3c209ab853010a605dd1
uhbrar <abrar.mesbah@gmail.com>
2025-08-04 09:51:09 -0400

    bump version

--- strider/server.py
+++ strider/server.py
@@ -74,7 +74,7 @@ openapi_args = dict(
     title="Strider",
     description=DESCRIPTION,
     docs_url=None,
-    version="4.8.4",
+    version="4.8.5",
     terms_of_service=(
         "http://robokop.renci.org:7055/tos"
         "?service_long=Strider"
